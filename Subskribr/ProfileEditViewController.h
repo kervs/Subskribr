92194a6c743d73b43b16f70fcc2dafb3be953a8f
kervs <kervins.valcourt@gmail.com>
2015-02-02 16:25:22 -0500

    profile view and profile edit view completed

---
+++ Subskribr/ProfileEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileEditViewController.h
+//  Subskribr
+//
+//  Created by Kervins Valcourt on 2/2/15.
+//  Copyright (c) 2015 EastoftheWestEnd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileEditViewController : UIViewController
+
+@end
---
+++ Subskribr/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Subskribr
+//
+//  Created by Kervins Valcourt on 2/2/15.
+//  Copyright (c) 2015 EastoftheWestEnd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
